c850c9a622585dd81006d97c8d73712d86894aa2
evalle <shmarnev@gmail.com>
2017-01-10 16:47:27 +0100

    Hangman is ready to serve

--- hangman.py
+++ hangman.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 import random
+import sys
 
-words = ['titanic']  # , 'diehard', 'starwars', 'startrek']
+words = ['titanic', 'diehard', 'starwars', 'startrek']
 
 def main():
     
-    guesses = 2
+    guesses = 7
     keyword = random.choice(words)
-
     keyword_splitted = list(keyword)
     keyword_on_screen = ''.join(list('-' * len(keyword)))
     guessed_letters = list()
+
     print(keyword_on_screen)
 
-    while guesses > 0:
+    while guesses > 0: 
         user_letter = input('=> ')
         
         if user_letter in keyword:
@@ -27,11 +28,17 @@ def main():
                 keyword_on_screen.append(i)
             keyword_on_screen = (''.join(keyword_on_screen))
             print(keyword_on_screen)
+            
+            if keyword_on_screen == keyword:
+                print("You won! The word was %s" % keyword)
+                sys.exit(0)
+
         else:
             guesses -= 1
 
             if guesses == 0:
                 print("Sorry, you don't have gueeses anymore")
+                print("The word was %s" % keyword)
             else:
                 print('Guesses left: %d' % guesses)
 
